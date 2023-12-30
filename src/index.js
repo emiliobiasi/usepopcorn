5f9a31d40db2786e431ee4e3717c17eb3ae95acb
Emílio Biasi <ejbiasi@hotmail.com>
2023-12-30 17:56:30 -0300

    ajustando para deploy

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie has rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie has rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
